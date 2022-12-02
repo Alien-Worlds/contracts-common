d08be8fb7ad8c85d4e3b959e94f76ff969a47762
Angelo Laub <angelolaub@gmail.com>
2022-12-02 13:31:31 +0100

    remove optimistic locking

--- singleton.hpp
+++ singleton.hpp
@@ -4,10 +4,6 @@
 #include <eosio/singleton.hpp>
 #include <eosio/time.hpp>
 
-#ifdef IS_DEV
-#define USE_OPTIMISTIC_LOCKING
-#endif
-
 /**
  * simple getter/setter
  **/
@@ -39,59 +35,35 @@
         unset(#name);                                                                                                  \
     }
 
-#ifdef USE_OPTIMISTIC_LOCKING
 #define SINGLETON(table_name, contract_name, ...)                                                                      \
     struct table_name##_struct;                                                                                        \
     using table_name##_singleton = eosio::singleton<#table_name##_n, table_name##_struct>;                             \
-    struct [[eosio::table(#table_name), eosio::contract(#contract_name)]] table_name##_struct                          \
-        : public SingletonStruct {                                                                                     \
-        EOSLIB_SERIALIZE_DERIVED(table_name##_struct, SingletonStruct, (data)(serial));                                \
+    struct [[eosio::table(#table_name), eosio::contract(#contract_name)]] table_name##_struct {                        \
+        std::map<std::string, state_value_variant> data = {};                                                          \
     };                                                                                                                 \
     struct table_name : Singleton<table_name##_singleton, table_name##_struct> {                                       \
         using Singleton::Singleton;                                                                                    \
         __VA_ARGS__                                                                                                    \
     };
-#else
-#define SINGLETON(table_name, contract_name, ...)                                                                      \
-    struct table_name##_struct;                                                                                        \
-    using table_name##_singleton = eosio::singleton<#table_name##_n, table_name##_struct>;                             \
-    struct [[eosio::table(#table_name), eosio::contract(#contract_name)]] table_name##_struct                          \
-        : public SingletonStruct {                                                                                     \
-        EOSLIB_SERIALIZE_DERIVED(table_name##_struct, SingletonStruct, (data));                                        \
-    };                                                                                                                 \
-    struct table_name : Singleton<table_name##_singleton, table_name##_struct> {                                       \
-        using Singleton::Singleton;                                                                                    \
-        __VA_ARGS__                                                                                                    \
-    };
-
-#endif
 
 using state_value_variant = std::variant<int8_t, uint8_t, int32_t, uint32_t, int64_t, uint64_t, int128_t, uint128_t,
     bool, std::vector<int64_t>, eosio::name, std::string, eosio::time_point_sec, eosio::asset, eosio::extended_asset>;
-
-struct SingletonStruct {
-    std::map<std::string, state_value_variant> data = {};
-#ifdef USE_OPTIMISTIC_LOCKING
-    uint8_t serial;
-
-    EOSLIB_SERIALIZE(SingletonStruct, (data)(serial))
-#else
-    EOSLIB_SERIALIZE(SingletonStruct, (data))
-#endif
-};
 
 template <typename Table, typename Struct>
 struct Singleton {
   private:
     eosio::name contract;
-    eosio::name scope;
+    uint64_t    scope;
     Table       table;
     Struct      row;
     bool        dirty = false;
 
   public:
     explicit Singleton(const eosio::name &c, const eosio::name &s)
-        : contract(c), scope(s), table(c, s.value), row(table.get_or_default()) {}
+        : contract(c), scope(s.value), table(c, scope), row(table.get_or_default()) {}
+
+    explicit Singleton(const eosio::name &c, const uint64_t &s)
+        : contract(c), scope(s), table(c, scope), row(table.get_or_default()) {}
 
     ~Singleton() {
         if (dirty) {
@@ -101,13 +73,6 @@ struct Singleton {
 
   protected:
     void save() {
-#ifdef USE_OPTIMISTIC_LOCKING
-        // optimistic locking, prevents 2 different instances of overwriting each other's changes
-        auto current_table = Table{contract, scope.value};
-        auto current       = current_table.get_or_default();
-        check(current.serial == row.serial, "Table has been modified by another instance");
-        row.serial++;
-#endif
         table.set(row, contract);
     };
 
