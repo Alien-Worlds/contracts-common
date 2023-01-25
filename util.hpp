2121e2a553658a7e5635e6b8e9dce965cf188c36
Angelo Laub <angelolaub@gmail.com>
2023-01-25 17:17:52 +0100

    add overload to upsert helper function that accept iterator instead of pk

--- util.hpp
+++ util.hpp
@@ -82,8 +82,13 @@ void check(const bool pred, const std::string_view format, Args const &...args) 
 template <typename Table, typename Function>
 inline bool upsert(Table &table, const uint64_t pk, const eosio::name payer, const Function &updater) {
     const auto itr = table.find(pk);
+    return upsert(table, itr, payer, updater);
+}
+
+template <typename Table, typename Iterator, typename Function>
+inline bool upsert(Table &table, Iterator &itr, const eosio::name payer, const Function &updater) {
     if (itr == table.end()) {
-        table.emplace(payer, updater);
+        itr = table.emplace(payer, updater);
         return true;
     } else {
         table.modify(itr, payer, updater);
