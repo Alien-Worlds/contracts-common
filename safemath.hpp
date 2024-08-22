98913b02fe81e8f0690e6e918b64c11fda8aab74
Angelo Laub <angelolaub@gmail.com>
2024-08-22 20:09:51 +0400

    fix division by zero

--- safemath.hpp
+++ safemath.hpp
@@ -242,14 +242,14 @@ class S {
             ::check(!isinf(n), "infinity");
             ::check(!isnan(n), "NaN");
         } else if constexpr (std::is_unsigned_v<T>) {
-            ::check(n == 0 || n <= max() / a.n, "unsigned multiplication overflow");
+            ::check(a.n == 0 || n <= max() / a.n, "unsigned multiplication overflow");
             n *= a.n;
         } else {
             if (n > 0) {
                 if (a.n > 0) {
                     ::check(n <= max() / a.n, "signed multiplication overflow");
                 } else {
-                    ::check(a.n >= min() / n, "signed multiplication underflow");
+                    ::check(n == 0 || a.n >= min() / n, "signed multiplication underflow");
                 }
             } else {
                 if (a.n > 0) {
